17db3de6bafcbb74694fd5bcaf5256096a68fc00
kaindy7633 <kaindy7633@gmail.com>
2024-12-14 14:56:06 +0800

    完成随机密码强度的校验

--- src/process/gen_pass.rs
+++ src/process/gen_pass.rs
@@ -5,6 +5,19 @@ const LOWER: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
 const NUMBER: &[u8] = b"123456789";
 const SYMBOL: &[u8] = b"!@#$%^&*_";
 
+/// 生成随机密码
+///
+/// 根据指定的长度和字符类型生成随机密码，并打印出密码和密码强度评估。
+///
+/// # 参数
+/// * `length` - 密码的长度
+/// * `upper` - 是否包含大写字母
+/// * `lower` - 是否包含小写字母
+/// * `number` - 是否包含数字
+/// * `symbol` - 是否包含符号
+///
+/// # 返回值
+/// * `anyhow::Result` - 如果生成密码成功，返回 Ok(());否则返回错误信息
 pub fn process_genpass(
     length: u8,
     upper: bool,
@@ -16,6 +29,7 @@ pub fn process_genpass(
     let mut password = Vec::new();
     let mut chars = Vec::new();
 
+    // 根据参数选择性地添加字符集
     if upper {
         chars.extend_from_slice(UPPER);
         password.push(*UPPER.choose(&mut rng).expect("UPPER won't be empty"))
@@ -36,6 +50,7 @@ pub fn process_genpass(
         password.push(*SYMBOL.choose(&mut rng).expect("SYMBOL won't be empty"))
     }
 
+    // 填充密码到指定长度
     for _ in 0..(length - password.len() as u8) {
         let c = chars
             .choose(&mut rng)
@@ -43,9 +58,16 @@ pub fn process_genpass(
         password.push(*c);
     }
 
+    // 打乱密码顺序
     password.shuffle(&mut rng);
 
-    println!("{}", String::from_utf8(password)?);
+    // 将密码转换为字符串
+    let password = String::from_utf8(password)?;
+    println!("{}", password);
+
+    // 评估密码强度
+    let estimate = zxcvbn::zxcvbn(password.as_str(), &[]);
+    eprintln!("Password strength: {}", estimate.score());
 
     Ok(())
 }
